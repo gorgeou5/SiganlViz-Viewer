39e985660e0a963d9397d946a64f67306184b301
Sieun Choi <si6229@gmail.com>
2025-07-28 22:18:28 +0900

    다섯개 input, 그래프 merge 수정되어야함

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+  data: function() {
+    return {
+      name: '뷰',
+      type: 'javascript',
+    };
+  },
+}
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      {{ name }}
+    </div>
+  </header>
+  <main>
+    {{ type }}
+</main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  color: red;
+}
+
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
